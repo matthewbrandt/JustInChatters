208190aedf5267b8b41161249c30aaf448dc29d2
Matthew Brandt <matthewbrandt@gmail.com>
2021-11-21 17:30:45 +0100

    added icons

--- app.js
+++ app.js
@@ -1,40 +1,22 @@
 const broadcaster = 'matty_twoshoes'
 const url = `https://jwalter-chatters.builtwithdark.com/?broadcaster=${broadcaster}`
 
-fetch(url).then(function (response) {
-    // The API call was successful!
-    if (response.ok) {
-        return response.json();
-    } else {
-        return Promise.reject(response);
-    }
-}).then(function (data) {
-    for (key in data.chatters) {
-        const userType = key;
-        const excludeUserTypes = ['admins','broadcaster','global_mods'];
-        if (data.chatters[userType].length > 0 && excludeUserTypes.indexOf(userType) == -1) {
-            if (userType !== 'viewers') {
-                let divItem = document.createElement("div");
-                divItem.innerHTML = `<h2>${userType}</h2>`;
-                divItem.classList.add('row', userType);
-                let unorderedList = document.createElement("ul");
-                document.getElementById('chatters').append(divItem);
-                divItem.append(unorderedList);
-                for (let i = 0; i < data.chatters[userType].length; i++) {
-                    let listItem = document.createElement("li");
-                    let user = data.chatters[userType][i];
-                    listItem.textContent = user;
-                    unorderedList.appendChild(listItem);   
-                }
-                
-                
-            }
-            else {
-                // add bots later
-                const friendlyBots = [];
-                const knownBots = [];
-                
-                let divItem = document.createElement("div");
+function getChatters() {
+    fetch(url).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(function (data) {
+        document.getElementById('chatters').innerHTML = '';
+        console.log(data.chatters);
+        for (key in data.chatters) {
+            const userType = key;
+            const excludeUserTypes = ['admins','broadcaster','global_mods'];
+            if (data.chatters[userType].length > 0 && excludeUserTypes.indexOf(userType) == -1) {
+                if (userType !== 'viewers') {
+                    let divItem = document.createElement("div");
                     divItem.innerHTML = `<h2>${userType}</h2>`;
                     divItem.classList.add('row', userType);
                     let unorderedList = document.createElement("ul");
@@ -46,11 +28,43 @@ fetch(url).then(function (response) {
                         listItem.textContent = user;
                         unorderedList.appendChild(listItem);   
                     }
+                    
+                    
+                }
+                else {
+                    // add bots later
+                    const friendlyBots = [];
+                    const knownBots = [];
+                    
+                    let divItem = document.createElement("div");
+                        divItem.innerHTML = `<h2>${userType}</h2>`;
+                        divItem.classList.add('row', userType);
+                        let unorderedList = document.createElement("ul");
+                        document.getElementById('chatters').append(divItem);
+                        divItem.append(unorderedList);
+                        for (let i = 0; i < data.chatters[userType].length; i++) {
+                            let listItem = document.createElement("li");
+                            let user = data.chatters[userType][i];
+                            listItem.textContent = user;
+                            unorderedList.appendChild(listItem);   
+                        }
+                }
             }
-        }
-    };
-    
-}).catch(function (err) {
-    // there was an error
-    console.warn('Something went wrong!', err);
-});
+        };
+        
+    }).catch(function (err) {
+        // there was an error
+        console.warn('Something went wrong! ', err);
+    });
+
+    setTimeout(function() {
+
+      // again
+      getChatters();
+
+      // every 3 mins
+    }, 180000);
+}
+
+// Begins
+getChatters();
